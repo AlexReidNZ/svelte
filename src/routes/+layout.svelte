164bf16078aa7a8a31a95eef869f4d6ede453ec8
AlexReidNZ <reidaj7@student.op.ac.nz>
2023-05-10 14:22:30 +1200

    chore: Remove unused GiantBomb fetch, add unsplash fetch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,24 +2,44 @@
   import "../style.css";
   import Header from "$lib/header.svelte";
   import Footer from "$lib/footer.svelte";
+
+//Fetch for getting "gaming" images from unsplash
+
+  // let imagesArray = [];
+
+  // const BASE_URL = "https://api.unsplash.com/";
+  // fetch(
+  //   `${BASE_URL}/search/photos/?client_id=skDU48Yd4CUD6X2AaBJDlof39G4ykmRrVtvUKKLG2Ks&query=gaming`
+  // )
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     data.results.forEach((element) => {
+  //       imagesArray.push(element.urls.small);
+  //     });
+  //   });
 </script>
 
 <Header />
 <main>
-  <slot />
-  <!-- page specific content -->
+  <div>
+    <slot />
+    <!-- page specific content -->
+  </div>
 </main>
 <Footer />
 
 <style>
   main {
+    background-color: var(--lightColour);
+    background-repeat: no-repeat;
+  }
+  div {
+    width: 50vw;
     min-height: 85vh;
     color: var(--bodyText);
     background-color: var(--lightColour);
     background-size: 100%;
-    margin: 0px;
-  }
-  main{
+    margin: auto;
     padding-top: 10vh;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <script>
-  // const BASE_URL = "https://www.giantbomb.com/api";
-
-  // fetch(
-  //   `${BASE_URL}/games/?api_key=cb638b4d991587308456a3533f0914df1b87309d&format=json&field_list=name`
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //   });
+  
 </script>
 
 <section>
